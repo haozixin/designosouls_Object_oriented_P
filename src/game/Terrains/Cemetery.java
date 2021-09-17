e19eecbe601144cc3352613a6dc280938c15b227
Zixin Hao <zhao0004@student.monash.edu>
2021-09-17 09:06:20 +0800

    working on requirement 5

---
+++ src/game/Terrains/Cemetery.java
@@ -0,0 +1,17 @@
+package game.Terrains;
+
+import edu.monash.fit2099.engine.Ground;
+
+public class Cemetery extends Ground {
+
+    /**
+     * Constructor.
+     *
+     * @param displayChar character to display for this type of terrain
+     */
+    public Cemetery() {
+        super('c');
+    }
+
+
+}
