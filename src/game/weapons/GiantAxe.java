c536ec3576decbeb01be52cd036f80ce96e87838
Bryan Daaverel <bdav0011@student.monash.edu>
2021-09-26 13:13:17 +0700

    Finish Giant Axe class and add action spin attack action where the giant axe can deal half its original damage to surrounding adjacent of the user

--- src/game/weapons/GiantAxe.java
+++ src/game/weapons/GiantAxe.java
@@ -1,27 +1,41 @@
 package game.weapons;
 
-
-import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Weapon;
-import edu.monash.fit2099.engine.WeaponAction;
-import game.actions.SpinAttackAction;
+//import game.interfaces.EnemyWeapon;
+import edu.monash.fit2099.engine.Actor;
+import edu.monash.fit2099.engine.GameMap;
+import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class GiantAxe extends MeleeWeapon implements Weapon{
-    private int successRate;
+public class GiantAxe extends MeleeWeapon implements Weapon {
+    private int price;
 
-
+    /**
+     * Constructor to make Giant Axe
+     */
     public GiantAxe() {
         super("GiantAxe", 'G', 50, "Split to", 80);
-        setPrice(1000);
+        this.price = 1000;
+    }
 
+    /**
+     *
+     * @return price
+     */
+    public int getPrice() {
+        return price;
+    }
+
+    /**
+     * special skill where it deals AOE damage that deals half the damage to its adjacent square
+     */
+    public void areaDamage() {
+        damage = damage / 2;
     }
 
 
-
-
-    @Override
-    public WeaponAction getActiveSkill(Actor target, String direction) {
-        return new SpinAttackAction(this);
-    }
 }
